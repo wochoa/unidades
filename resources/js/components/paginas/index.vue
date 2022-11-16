c89d1e0b8bf92b4dd6b223ca0efd8f53cfe9484d
wochoa <woalvarado1@gmail.com>
2022-11-15 21:21:19 -0500

    v2

--- resources/js/components/paginas/index.vue
+++ resources/js/components/paginas/index.vue
@@ -31,9 +31,9 @@
                     <!-- SECCION 2 -->
                     <div class="row">
 
-                        <div class="col-sm-4" v-for="(sec2,indexs2) in listasecciones" v-if="sec2.seccion_pag==2" v-html="sec2.titulo">
+                        <template  v-for="(sec2,indexs2) in listasecciones" v-if="sec2.seccion_pag==2" v-html="sec2.titulo">
 
-                        </div>
+                        </template>
 
                     </div>
 
